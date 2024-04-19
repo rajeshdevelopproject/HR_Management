f8b603110a72d5cc0782caa36d5b6764a60ba850
Rajeshkumar <rajeshkumar210730@gmail.com>
2024-04-19 12:59:56 +0530

    [add]static value

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+export function ApplicationData(){
+    return{
+
+    }
+}
+
+export function TodayInterview(){
+    return[]
+}
+
+export function PostedJobs(){
+
+}
+
+export function CandidateStatus(){
+
+}
+export function UpComingData(){
+    
+}
+export function Activity(){
+    
+}
+export function HiringCandidates(){
+    
+}
